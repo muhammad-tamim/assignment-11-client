d01bc9e6a6c18dc490829a383fbef5b3379e010c
muhammad-tamim <contact2tamim@gmail.com>
2025-06-16 01:34:04 +0600

    style(RecentListingsCard): remove fixed height

--- src/component/Home/RecentListingsCard.jsx
+++ src/component/Home/RecentListingsCard.jsx
@@ -4,7 +4,7 @@ const RecentListingsCard = ({ car, getTimeAgo }) => {
     const { imageUrl, carModel, rentalPrice, availability, bookingCount, postedDate } = car;
     return (
         <div className='dark:bg-gray-50 dark:text-gray-800 rounded-lg shadow-md p-4 hover:shadow-xl hover:scale-105 transition duration-300'>
-            <img src={imageUrl} alt={carModel} className='w-full h-48 rounded mb-4' />
+            <img src={imageUrl} alt={carModel} className='w-full rounded mb-4' />
             <h2 className='text-xl font-semibold'>{carModel}</h2>
             <p className='text-gray-600'>Rental Price: {rentalPrice}/day</p>
             <p>
