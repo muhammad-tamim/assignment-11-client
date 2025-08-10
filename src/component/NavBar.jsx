16631cd523bb158f55125621d2ebb6a3ddfd3c45
muhammad-tamim <contact2tamim@gmail.com>
2025-08-10 13:07:58 +0600

    style(NavBar.jsx): impove navbar styles

--- src/component/NavBar.jsx
+++ src/component/NavBar.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext } from 'react';
 import { Link, NavLink } from 'react-router';
 import logo from '../assets/logo.png'
 import { context } from '../layout/RootLayout';
@@ -15,17 +15,20 @@ const Navbar = () => {
     const handleSubmit = () => {
         handleSignOut()
     }
+
     return (
-        <div className="max-w-screen-2xl mx-auto navbar px-4 lg:px-10">
+        <div className="px-4 lg:px-10 navbar bg-base-100 border-b border-base-300 shadow-sm">
             <div className="navbar-start">
                 {/* Drop Down For Small Devices */}
                 <div className="dropdown">
-                    <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
+                    <div tabIndex={0} role="button" className="btn bg-base-300 btn-sm lg:hidden">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" />
+                        </svg>
                     </div>
                     <ul
                         tabIndex={0}
-                        className="menu menu-sm dropdown-content bg-base-100  z-1 mt-3 w-52 p-2 shadow text-primary">
+                        className="menu menu-sm dropdown-content bg-base-100 z-1 mt-3 w-52 p-2 shadow text-primary">
                         <li className='hover:text-primary'>
                             <NavLink to="/" className={({ isActive }) => isActive ? "text-primary" : ''}>Home</NavLink>
                         </li>
@@ -33,18 +36,8 @@ const Navbar = () => {
                             <NavLink to="/AvailableCars" className={({ isActive }) => isActive ? "text-primary" : ''}>Available Cars</NavLink>
                         </li>
                         {
-                            !user ? (
-                                <li className='hover:text-primary'>
-                                    <NavLink
-                                        to="/signin"
-                                        className={({ isActive }) => isActive ? "text-primary" : ''}
-                                    >
-                                        SignIn
-                                    </NavLink>
-                                </li>
-                            )
-                                :
-                                <ul className="">
+                            user && (
+                                <ul>
                                     <li className='hover:text-primary'>
                                         <NavLink to="/AddCar" className={({ isActive }) => isActive ? "text-primary" : ''}>Add Car</NavLink>
                                     </li>
@@ -54,18 +47,18 @@ const Navbar = () => {
                                     <li className='hover:text-primary'>
                                         <NavLink to="/MyBookings" className={({ isActive }) => isActive ? "text-primary" : ''}>My Bookings</NavLink>
                                     </li>
-                                    <li className='hover:text-primary'>
-                                        <a onClick={() => handleSubmit()} className='cursor-pointer'>SignOut</a>
-                                    </li>
                                 </ul>
+                            )
                         }
                     </ul>
                 </div>
                 {/* Logo */}
-                <Link to="/" className='cursor-pointer flex items-center gap-1 size-20'><img src={logo} alt="logo.png" /></Link>
+                <Link to="/" className='cursor-pointer ml-2 lg:ml-0 flex items-center gap-1 size-20'>
+                    <img src={logo} alt="logo.png" />
+                </Link>
             </div>
 
-            {/* Bills and Profile */}
+            {/* Navbar center links */}
             <div className="navbar-center hidden lg:flex xl:text-xl">
                 <ul className="flex gap-5 px-1">
                     <li className='hover:text-primary'>
@@ -74,21 +67,9 @@ const Navbar = () => {
                     <li className='hover:text-primary'>
                         <NavLink to="/AvailableCars" className={({ isActive }) => isActive ? "text-primary" : ''}>Available Cars</NavLink>
                     </li>
-
                     {
-                        !user ? (
-                            <li className='hover:text-primary'>
-
-                                <NavLink
-                                    to="/signin"
-                                    className={({ isActive }) => isActive ? "text-primary" : ''}
-                                >
-                                    SignIn
-                                </NavLink>
-                            </li>
-                        )
-                            :
-                            <ul className="flex gap-5 px-1">
+                        user && (
+                            <>
                                 <li className='hover:text-primary'>
                                     <NavLink to="/AddCar" className={({ isActive }) => isActive ? "text-primary" : ''}>Add Car</NavLink>
                                 </li>
@@ -98,23 +79,32 @@ const Navbar = () => {
                                 <li className='hover:text-primary'>
                                     <NavLink to="/MyBookings" className={({ isActive }) => isActive ? "text-primary" : ''}>My Bookings</NavLink>
                                 </li>
-                                <li className='hover:text-primary'>
-                                    <a onClick={() => handleSubmit()} className='cursor-pointer'>SignOut</a>
-                                </li>
-                            </ul>
+                            </>
+                        )
                     }
                 </ul>
             </div>
 
+            {/* Navbar end - moved SignIn/SignOut here */}
             <div className="navbar-end gap-5 xl:text-xl">
-                <label className="toggle text-base-content">
-                    <input type="checkbox" value="dark" className="theme-controller" />
+                {
+                    !user ? (
+                        <NavLink
+                            to="/signin"
+                            className={({ isActive }) => isActive ? "text-primary" : 'hover:text-primary'}
+                        >
+                            SignIn
+                        </NavLink>
+                    ) : (
+                        <a
+                            onClick={() => handleSubmit()}
+                            className="cursor-pointer hover:text-primary"
+                        >
+                            SignOut
+                        </a>
+                    )
+                }
 
-                    <svg aria-label="sun" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><g strokeLinejoin="round" strokeLinecap="round" strokeWidth="2" fill="none" stroke="currentColor"><circle cx="12" cy="12" r="4"></circle><path d="M12 2v2"></path><path d="M12 20v2"></path><path d="m4.93 4.93 1.41 1.41"></path><path d="m17.66 17.66 1.41 1.41"></path><path d="M2 12h2"></path><path d="M20 12h2"></path><path d="m6.34 17.66-1.41 1.41"></path><path d="m19.07 4.93-1.41 1.41"></path></g></svg>
-
-                    <svg aria-label="moon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><g strokeLinejoin="round" strokeLinecap="round" strokeWidth="2" fill="none" stroke="currentColor"><path d="M12 3a6 6 0 0 0 9 9 9 9 0 1 1-9-9Z"></path></g></svg>
-
-                </label>
                 {/* If logged in show profile info */}
                 {
                     user &&
@@ -123,15 +113,14 @@ const Navbar = () => {
                             <div className="w-10 rounded-full">
                                 <img
                                     alt=""
-                                    src={user?.photoURL} />
+                                    src={user?.photoURL}
+                                />
                             </div>
                         </div>
                     </div>
                 }
-
             </div>
         </div>
-
     );
 };
 
