ec4095074c75d49cc2d8bc7a9db205c4c56cd56c
muhammad-tamim <contact2tamim@gmail.com>
2025-08-11 11:38:46 +0600

    style(Home Page): make the page look good on dark mode

--- src/component/Home/SpecialOffersCard.jsx
+++ src/component/Home/SpecialOffersCard.jsx
@@ -5,7 +5,6 @@ import { Link } from 'react-router';
 const SpecialOffersCard = ({ offer }) => {
     const { title, price, description, image } = offer;
 
-    // Map titles to their routes
     const routeMap = {
         'Economy Drive': '/economyDrive',
         'Business Elite': '/businessElite',
@@ -14,14 +13,14 @@ const SpecialOffersCard = ({ offer }) => {
     };
 
     return (
-        <div className="flex flex-col flex-grow p-6 space-y-6 rounded-xl shadow-md sm:p-8 
-                        dark:bg-gray-50 dark:text-gray-800 transform transition-transform 
+        <div className="border border-white flex flex-col flex-grow p-6 space-y-6 rounded-xl shadow-md sm:p-8 
+                        dark:bg-gray-50z transform transition-transform 
                         duration-300 hover:scale-105 hover:-translate-y-1">
             <div className="space-y-2">
                 <h4 className="text-2xl font-bold">{title}</h4>
                 <span className="text-6xl font-bold">{price}</span>
             </div>
-            <p className="leading-relaxed dark:text-gray-600">{description}</p>
+            <p className="leading-relaxed">{description}</p>
             <div>
                 <img
                     src={image}
